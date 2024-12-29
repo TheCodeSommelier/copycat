1662b38b6e7843fea13118825113be8f82683a53
Tony Masek <tm@tony-masek.com>
2024-12-30 00:27:04 +0100

    Delete comment

--- src/app.js
+++ src/app.js
@@ -7,7 +7,6 @@ import dotenv from "dotenv";
 import pd from "pretty-data";
 dotenv.config();
 
-// Hey one more time...
 const main = async () => {
   const imapClient = new ImapClient();
   await imapClient.connect();
