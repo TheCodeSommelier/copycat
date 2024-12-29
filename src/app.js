fa3dd4457bfcba84b25e80d7049f8c93eba224e0
Tony Masek <tm@tony-masek.com>
2024-12-30 00:11:03 +0100

    Add comment to try to push to contabo

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ import dotenv from "dotenv";
 import pd from "pretty-data";
 dotenv.config();
 
+// Hey!!
 const main = async () => {
   const imapClient = new ImapClient();
   await imapClient.connect();
